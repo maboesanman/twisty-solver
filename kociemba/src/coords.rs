50b9f551557da423651557beed77f3241649ca43
Mason Boeman <masonboeman@gmail.com>
2022-12-05 01:59:05 +0000

    more updates

--- kociemba/src/coords.rs
+++ kociemba/src/coords.rs
@@ -1,10 +1,11 @@
 use deku::prelude::*;
+use paste::paste;
 
 macro_rules! define_coord {
-    ($name:ident, $inner:ty, $bits:expr) => {
+    ($name:ident, $inner:ty, $range:expr, $bits:expr) => {
         #[derive(Debug, PartialEq, DekuRead, DekuWrite, Copy, Clone)]
         #[deku(endian = "big")]
-        pub struct $name(
+        pub struct $name (
             #[deku(bits = $bits)]
             $inner
         );
@@ -23,54 +24,91 @@ macro_rules! define_coord {
     }
 }
 
+macro_rules! define_raw_coord_no_table {
+    ($name:ident, $inner:ty, $range:expr, $bits:expr) => {
+        paste! {
+            define_coord!([<$name Coord>], $inner, $range, $bits);
+        }
+    }
+}
+
+macro_rules! define_raw_coord {
+    ($name:ident, $inner:ty, $range:expr, $bits:expr) => {
+        paste! {
+            define_raw_coord_no_table!($name, $inner, $range, $bits);
+
+            // struct [<$name MoveTable>] {
+            //     transforms: [[<$name Coord>]; 15],
+            //     moves: [[<$name Coord>]; 18],
+            // }
+        }
+    }
+}
+
+macro_rules! define_sym_coord {
+    ($name:ident, $inner:ty, $range:expr, $bits:expr, $moves:expr) => {
+        paste! {
+            define_coord!([<$name SymCoord>], $inner, $range, $bits);
+
+            // struct [<$name SymMoveTable>] {
+            //     moves: [([<$name SymCoord>], Transform); $moves],
+            // }
+        }
+    }
+}
+
+// transforms
+
+define_coord!(Transform, u8, 16, 4);
+
 // raw coordinates
 
 // 2187 (11.09 bits)
 // tables: (866,052 bits, 108kb)
 // - all moves
 // - all transforms
-define_coord!(CornerOrientCoord, u16, 12);
+define_raw_coord!(CornerOrient, u16, 2187, 12);
 
 // 2048 (11 bits) u16
 // tables: (743,424 bits, 93kb)
 // - all moves
 // - all transforms
-define_coord!(EdgeOrientCoord, u16, 11);
+define_raw_coord_no_table!(EdgeOrient, u16, 2048, 11);
 
 // 495 (8.9 bits) u16
 // tables: (147,015 bits, 18kb)
 // - all moves
 // - all transforms
-define_coord!(EdgeGroupCoord, u16, 9);
+define_raw_coord_no_table!(EdgeGroup, u16, 495, 9);
 
 // 40320 (15.29 bits) u16
 // tables: (21,288,960 bits, 2,661kb)
 // - all moves
 // - all transforms
-define_coord!(CornerPermCoord, u16, 16);
+define_raw_coord!(CornerPerm, u16, 40320, 16);
 
 // 40320 (15.29 bits) u16
 // tables: (21,288,960 bits, 2,661kb)
 // - all moves
 // - all transforms
-define_coord!(UDEdgePermCoord, u16, 16);
+define_raw_coord!(UDEdgePerm, u16, 40320, 16);
 
 // 24 (4.58 bits) u8
 // tables: (3600 bits, 450 bytes)
 // - all moves
 // - all transforms
-define_coord!(EEdgePermCoord, u8, 5);
+define_raw_coord!(EEdgePerm, u8, 24, 5);
 
 // sym coordinates
 
 // 64430 (15.97 bits) u16
 // tables: (23,194,800 bits, 2,899kb)
 // - all moves + transform index
-define_coord!(EdgeOrientGroupSymCoord, u16, 16);
+define_sym_coord!(EdgeOrientGroup, u16, 64430, 16, 18);
 
 // 2768 (11.43 bits) u16
 // tables: (442,880 bits, 55kb)
 // - all moves + transform index
-define_coord!(CornerPermSymCoord, u16, 12);
+define_sym_coord!(CornerPerm, u16, 2768, 12, 10);
 
 // total table size: 8.5 mb
--- kociemba/src/permutation_coord.rs
+++ kociemba/src/permutation_coord.rs
@@ -1,115 +1,97 @@
+use paste::paste;
+
 macro_rules! permutation_coord {
-    ($n:expr, $t:ty,
-        $id: ident,
-        $id_inv: ident,
-        $id_test: ident,
-        $id_factorials: ident,
-        $id_first_perm: ident,
-    ) => {
-        const $id_factorials: [$t; $n + 1] = {
-            let mut f: [$t; $n + 1] = [1; $n + 1];
-            let mut i = 1;
-            loop {
-                f[i] = f[i - 1] * (i as $t);
-                i += 1;
-                if i == $n {
-                    break;
-                }
-            }
-            f
-        };
-
-        const $id_first_perm: [u8; $n] = {
-            let mut f: [u8; $n] = [0; $n];
-            let mut i = 0;
-            loop {
-                f[i] = i as u8;
-                i += 1;
-                if i == $n {
-                    break;
-                }
-            }
-            f
-        };
-
-        pub const fn $id(perm: &[u8; $n]) -> $t {
-            let mut sum = 0;
-            let mut i = 1;
-            while i < $n {
-                let mut j = 0;
-                while j < i {
-                    if perm[j] > perm[i] {
-                        sum += $id_factorials[i]
-                    }
-
-                    j += 1;
-                }
-
-                i += 1;
-            }
-            sum
-        }
-
-        pub const fn $id_inv(mut coord: $t) -> [u8; $n] {
-            let mut f = $n - 1;
-            let mut result = $id_first_perm;
-            let mut c = 0;
-            loop {
-                if $id_factorials[f] <= coord {
-                    coord -= $id_factorials[f];
-                    c += 1;
-                } else {
-                    if c != 0 {
-                        let swap = result[f - c];
-                        let mut i = f - c;
-                        while i < f {
-                            result[i] = result[i + 1];
-                            i += 1;
-                        }
-                        // todo!();
-                        result[f] = swap;
-                        c = 0;
-                    }
-                    if f == 0 {
+    ($n:expr, $t:ty) => {
+        paste! {
+            const [<FACTORIALS_ $n>]: [$t; $n + 1] = {
+                let mut f: [$t; $n + 1] = [1; $n + 1];
+                let mut i = 1;
+                loop {
+                    f[i] = f[i - 1] * (i as $t);
+                    i += 1;
+                    if i == $n {
                         break;
                     }
-                    f -= 1;
                 }
+                f
+            };
+    
+            const [<FIRST_PERM_ $n>]: [u8; $n] = {
+                let mut f: [u8; $n] = [0; $n];
+                let mut i = 0;
+                loop {
+                    f[i] = i as u8;
+                    i += 1;
+                    if i == $n {
+                        break;
+                    }
+                }
+                f
+            };
+    
+            pub const fn [<permutation_coord_ $n>](perm: &[u8; $n]) -> $t {
+                let mut sum = 0;
+                let mut i = 1;
+                while i < $n {
+                    let mut j = 0;
+                    while j < i {
+                        if perm[j] > perm[i] {
+                            sum += [<FACTORIALS_ $n>][i]
+                        }
+    
+                        j += 1;
+                    }
+    
+                    i += 1;
+                }
+                sum
             }
-
-            result
-        }
-
-        #[test]
-        fn $id_test() {
-            // the domain is small enough so we just check the whole thing.
-            for i in 0..$id_factorials[$n] {
-                let s = $id_inv(i);
-                assert_eq!($id(&s), i);
+    
+            pub const fn [<permutation_coord_ $n _inverse>](mut coord: $t) -> [u8; $n] {
+                let mut f = $n - 1;
+                let mut result = [<FIRST_PERM_ $n>];
+                let mut c = 0;
+                loop {
+                    if [<FACTORIALS_ $n>][f] <= coord {
+                        coord -= [<FACTORIALS_ $n>][f];
+                        c += 1;
+                    } else {
+                        if c != 0 {
+                            let swap = result[f - c];
+                            let mut i = f - c;
+                            while i < f {
+                                result[i] = result[i + 1];
+                                i += 1;
+                            }
+                            // todo!();
+                            result[f] = swap;
+                            c = 0;
+                        }
+                        if f == 0 {
+                            break;
+                        }
+                        f -= 1;
+                    }
+                }
+    
+                result
+            }
+    
+            #[test]
+            fn [<permutation_coord_ $n _test>]() {
+                // the domain is small enough so we just check the whole thing.
+                for i in 0..[<FACTORIALS_ $n>][$n] {
+                    let s = [<permutation_coord_ $n _inverse>](i);
+                    assert_eq!([<permutation_coord_ $n>](&s), i);
+                }
             }
         }
     };
 }
 
-permutation_coord!(
-    8,
-    u16,
-    permutation_coord_8,
-    permutation_coord_8_inverse,
-    permutation_coord_8_test,
-    FACTORIALS_8,
-    FIRST_PERM_8,
-);
+permutation_coord!(8, u16);
 
-permutation_coord!(
-    4,
-    u8,
-    permutation_coord_4,
-    permutation_coord_4_inverse,
-    permutation_coord_4_test,
-    FACTORIALS_4,
-    FIRST_PERM_4,
-);
+permutation_coord!(4, u8);
 
 
 const COMBINATIONS: [[u16; 4]; 12] = {
